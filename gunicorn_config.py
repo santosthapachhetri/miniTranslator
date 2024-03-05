15113f1c6f77d90e82e6a8e00019928009d5d944
Santosh Thapa <santosthapachhetri@gmail.com>
2024-03-06 01:03:08 +0545

    Initial commit

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,20 +1,4 @@
-# gunicorn.config.py
-
-import multiprocessing
-
-bind = "0.0.0.0:8000"  # Set the host and port
-workers = multiprocessing.cpu_count() * 2 + 1  # Adjust the number of workers based on your needs
-reload = True
-preload_app = True
-# Enable automatic reloading on code changes during development
-worker_class = "gevent"  # Use the gevent worker class for better performance
-
-# Optional Gunicorn settings
-# timeout = 30
-# loglevel = "info"
-# accesslog = "-"  # Log to stdout
-
-d# gunicorn_config.py
+# gunicorn_config.py
 
 def post_fork(server, worker):
     from multiprocessing import util
@@ -26,3 +10,9 @@ def worker_init(worker):
 
     # Suppress ALSA error messages
     ctypes.util.find_library('sndfile') or CDLL('libsndfile.so.1', mode=1)
+
+# Disable Gunicorn from trying to access audio devices
+def pre_fork(server, worker):
+    import soundfile as sf
+    import sys
+    sys.modules["soundfile"] = sf
